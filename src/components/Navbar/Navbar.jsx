beb3cb6016c3b27c75cd5e9a9e1d5c38888e6add
Vladimir RM <shvdrummer@gmail.com>
2023-06-30 17:42:50 +0300

    Import icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,15 @@ const Navbar = () => {
   return (
     <nav className='navbar'>
         <div className="container">
-            
+            <div className="navbar-content">
+                <div className='brand-and-toggler flex flex-sb'>
+                    <Link  to='/' className='navbar-brand text-uppercase fw-7 text-white ls-2 fs-22'>
+                        Volodymyr
+                    </Link>
+                    <button type='button' className='navbar-open-btn text-white'></button>
+                </div>
+
+            </div>
         </div>
       
     </nav>
