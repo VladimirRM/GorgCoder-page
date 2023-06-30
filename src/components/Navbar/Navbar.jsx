3fcd095c8c9667aa1c7dfcff316259a17e776675
Vladimir RM <shvdrummer@gmail.com>
2023-06-30 18:03:35 +0300

    Copy li

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
-import {FaBars}  from 'react-icons'
+import {FaBars, FaTimes}  from 'react-icons/fa'
 
 const Navbar = () => {
   return (
@@ -12,7 +12,10 @@ const Navbar = () => {
                     <Link  to='/' className='navbar-brand text-uppercase fw-7 text-white ls-2 fs-22'>
                         Volodymyr
                     </Link>
-                    <button type='button' className='navbar-open-btn text-white'></button>
+                    <button type='button' className='navbar-open-btn text-white'>
+                        {<FaBars size={30} />}
+                        <div className='navbar-collapse'></div>
+                    </button>
                 </div>
 
             </div>
