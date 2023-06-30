5aec3902f54888d307baf8b05301045a88691992
Vladimir RM <shvdrummer@gmail.com>
2023-06-30 12:20:17 +0300

    Add main h1

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+// import '../App.css'
 
 const Header = () => {
   return (
@@ -7,6 +8,11 @@ const Header = () => {
        <div className="container flex">
         <div className="header-content">
             <h2 className='text-uppercase text-white op-07 fw-6 ls-2'>web design, branding, graphic </h2>
+            <h1 className='text-white fw-6 header-title'>Hello i'm <span className='text-brown'>
+              Web & Graphic Designer  </span> Living Kyiv Ukraine </h1>
+              <div className='btn-groups flex'>
+                
+              </div>
         </div>
        </div>
     </div>
