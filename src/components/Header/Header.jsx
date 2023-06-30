3eeb38a9c368b64b44616fdc07f2a778c1f5f8d2
Vladimir RM <shvdrummer@gmail.com>
2023-06-30 13:34:02 +0300

    Import Navbar in the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Header.css'
-// import '../App.css'
+import Navbar from '../Navbar/Navbar'
 
 const Header = () => {
   return (
     <div className='header flex flex-col'  id='header'>
+      <Navbar/>
        <div className="container flex">
         <div className="header-content">
             <h2 className='text-uppercase text-white op-07 fw-6 ls-2'>web design, branding, graphic </h2>
