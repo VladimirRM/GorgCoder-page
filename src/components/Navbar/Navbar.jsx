1d8b890c2ec23f7ce9facacf26627ec5ff51fa2f
Vladimir RM <shvdrummer@gmail.com>
2023-06-30 18:15:03 +0300

    add css for navbar-collapse

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,9 +13,28 @@ const Navbar = () => {
                         Volodymyr
                     </Link>
                     <button type='button' className='navbar-open-btn text-white'>
-                        {<FaBars size={30} />}
-                        <div className='navbar-collapse'></div>
+                        <FaBars size={30} />
                     </button>
+
+                        <div className='navbar-collapse'>
+                            <button  className='navbar-close-btn text-white'type='button' >
+                              <FaTimes size={30}/>
+                            </button>
+                            <ul className='navbar-nav'>
+                                <li className='nav-item'>
+                                    <Link to='/'  className='nav-link text-white ls-1 
+                                    text-uppercase fw-6 fs-22'>Home</Link>
+                                </li>
+                                <li className='nav-item'>
+                                    <Link to='/'  className='nav-link text-white ls-1 
+                                    text-uppercase fw-6 fs-22'>Home</Link>
+                                </li>
+                                <li className='nav-item'>
+                                    <Link to='/'  className='nav-link text-white ls-1 
+                                    text-uppercase fw-6 fs-22'>Home</Link>
+                                </li>
+                            </ul>
+                        </div>
                 </div>
 
             </div>
