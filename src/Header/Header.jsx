dc527ff558d83075fab30d74a2ce7798da2bb2ef
Vladimir RM <shvdrummer@gmail.com>
2023-06-30 11:16:05 +0300

    Create and import header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,8 +3,12 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className='header'  id='header'>
-      
+    <div className='header flex flex-col'  id='header'>
+       <div className="container flex">
+        <div className="header-content">
+            <h2 className='text-uppercase text-white op-07 fw-6 ls-2'>web design, branding, graphic </h2>
+        </div>
+       </div>
     </div>
   )
 }
